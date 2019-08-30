1b218688956899bc73d30bf44e949ee55e7eef55
amrha <48659223+amrha@users.noreply.github.com>
2019-08-30 22:30:46 +0100

    Update object.py

--- object.py
+++ object.py
@@ -1,19 +1,18 @@
 import numpy as np
 import os
 import importlib
-from keras import backend as k
 from keras.preprocessing import image
-from keras.applications import mobilenet
+from keras.applications import mobilenet_v2
 from keras.applications import imagenet_utils
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
-mobile=mobilenet.MobileNet()
+mobile=mobilenet_v2.MobileNetV2()
 mobile._make_predict_function()
 def prepare(file):
 	img =image.load_img(file, target_size=(224,224))
 	img_array = image.img_to_array(img)
 	image_array_expanded_dims = np.expand_dims(img_array, axis=0)
-	return(mobilenet.preprocess_input(image_array_expanded_dims))
+	return(mobilenet_v2.preprocess_input(image_array_expanded_dims))
 def itl(path):
 	l=[]
 	preprocessed_image = prepare(path)
